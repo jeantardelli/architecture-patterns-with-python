c9d0d266a72da930c76d799b49e05f79cb5ef3b3
jeantardelli <jeantardelli@gmail.com>
2021-04-13 22:22:53 +0200

    Repository Pattern

--- model.py
+++ model.py
@@ -16,7 +16,7 @@ def allocate(line: OrderLine, batches: List[Batch]) -> str:
     except StopIteration:
         raise OutOfStock(f"Out of stock for sku {line.sku}")
 
-@dataclass(frozen=True)
+@dataclass(unsafe_hash=True)
 class OrderLine:
     orderid: str
     sku: str
