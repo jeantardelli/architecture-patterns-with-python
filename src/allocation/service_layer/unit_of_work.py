9cf617848e03fd546c7199feea5165437cbfea22
jeantardelli <jeantardelli@gmail.com>
2021-05-17 22:11:39 +0200

    All Message Bus - End Refactoring Service, and adjusting Model, App and Adaptors to use it

--- src/allocation/service_layer/unit_of_work.py
+++ src/allocation/service_layer/unit_of_work.py
@@ -20,13 +20,12 @@ class AbstractUnitOfWork(abc.ABC):
 
     def commit(self):
         self._commit()
-        self.publish_events()
+        self.collect_new_events()
 
-    def publish_events(self):
+    def collect_new_events(self):
         for product in self.products.seen:
             while product.events:
-                event = product.events.pop(0)
-                messagebus.handle(event)
+                yield product.events.pop(0)
 
     @abc.abstractmethod
     def _commit(self):
